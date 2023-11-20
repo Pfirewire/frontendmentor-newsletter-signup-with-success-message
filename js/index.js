dadc2f0f5e3f3b46b858c4af716bed4da0b60c78
Steve Pflug <stephen@pflug.us>
2023-11-20 11:07:36 -0600

    added modal styling and functionality

--- js/index.js
+++ js/index.js
@@ -1,20 +1,41 @@
 (() => {
-    console.log("Inside index.js");
+    console.log('Inside index.js');
 
+    // Globals
 
+    const body = document.querySelector('body');
+    const pageWrapper = document.querySelector('.page-wrapper');
+    const signupForm = document.querySelector('form');
+    const formInput = document.querySelector('form input');
+    const modal = document.querySelector('.modal');
+    const modalCard = document.querySelector('.modal-card');
+    const modalBtn = document.querySelector('.modal button');
+    const emailSpan = document.querySelector('.email-span');
 
+    // Functions
 
+    const handleSignupFormSubmit = e => {
+        console.log('Form submitted');
+        e.preventDefault();
+        emailSpan.innerText = formInput.value;
+        modal.style.display = 'flex';
+        pageWrapper.style.filter = 'blur(5px)';
+    };
+    const handleCloseModal = e => {
+        if((e.target !== modalCard && !modalCard.contains(e.target)) || e.target === modalBtn) {
+            console.log('clicked outside of modal card');
+            modal.style.display = 'none';
+            pageWrapper.style.filter = 'blur(0)';
+        } else {
+            console.log('clicked inside modal card');
+        }
+    };
 
+    // Events
 
+    signupForm.addEventListener('submit', handleSignupFormSubmit);
+    body.addEventListener('click', handleCloseModal);
 
-
-
-
-
-
-
-
-
-
+    // On Load
 
 })();
